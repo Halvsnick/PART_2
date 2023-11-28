3695a0e8dea68b9820cdd08d1da5801cf16c4da8
Natàlia Cortés Danés <1668991@uab.cat>
2023-11-28 11:58:48 +0100

    començament

--- LP-OSM-DLL/BallTree.cpp
+++ LP-OSM-DLL/BallTree.cpp
@@ -4,6 +4,11 @@
 void BallTree::construirArbre(const std::vector<Coordinate>& coordenades) {
     // TODO: Utilitza aquest metode per construir el teu BallTree
     // TODO: amb les coordenades que se us passen per parametre
+
+   //anar agafant els camins un per un i afegir totes les coordenades en un greaf de coordinate 
+    //nose si fer un graf o una llista desordenada i despres ja apanyarme *mirar 
+
+
 }
 
 void BallTree::inOrdre(std::vector<std::list<Coordinate>>& out) {
